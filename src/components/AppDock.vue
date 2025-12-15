ab2d572e571f308aeddb016adfed8bd62d2c6a9b
sooooooooooooooooootheby <therese.sjdyka@gmail.com>
2025-12-15 08:33:10 +0800

    feat: 隐藏收藏button

--- src/components/AppDock.vue
+++ src/components/AppDock.vue
@@ -4,10 +4,10 @@
 			<Icon icon="gravity-ui:house" class="text-xl" />
 			<span class="dock-label">主页</span>
 		</router-link>
-		<router-link to="/collect" class="dock-item" :class="{ 'dock-active': router.currentRoute.value.path === '/collect' }">
+		<!-- <router-link to="/collect" class="dock-item" :class="{ 'dock-active': router.currentRoute.value.path === '/collect' }">
 			<Icon icon="gravity-ui:tray" class="text-xl" />
 			<span class="dock-label">收藏</span>
-		</router-link>
+		</router-link> -->
 		<router-link to="/setting" class="dock-item" :class="{ 'dock-active': router.currentRoute.value.path === '/setting' }">
 			<Icon icon="gravity-ui:gear" class="text-xl" />
 			<span class="dock-label">设置</span>
